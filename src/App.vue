48f8647f0841724c71b050e5380b6c47ef2bf997
haozheng <1741167304@qq.com>
2022-09-26 12:13:50 +0800

    cnode

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
     <div id="app">
         <Header></Header>
-        <PostList></PostList>
+        <div class="main">
+     <router-view name="main"></router-view>
+        </div>
     </div>
 </template>
 
 <script>
 
     import Header from './components/Header'
-    import PostList from "./components/postList";
+/*    import PostList from "./components/PostList";
+import Article from "./components/Article";*/
     export default {
         name: 'App',
         components: {
             Header,
-            PostList,
         }
     }
 </script>
 
 <style>
-
+.main{
+    width: 80%;
+    margin: 0 auto;
+}
 </style>
